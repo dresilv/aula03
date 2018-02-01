3a3d996c35a59a2616f9f8b190b39748e67be0fb
aluno <Aluno@salas.aulas>
2018-01-31 22:39:40 -0200

    Contato teste alterado com uma nova condução de teste que verifica alteração

--- Agenda/src/test/br/edu/impacta/ads/ContatoTest.java
+++ Agenda/src/test/br/edu/impacta/ads/ContatoTest.java
@@ -24,4 +24,12 @@ public class ContatoTest {
 		cdao.inserir(contato);
 		assertTrue("O contato não foi adicionado.", cdao.existe(contato));
 	}
+	
+	@Test
+	public void testAndressa() {
+		int tamanhoanterior = cdao.lerTodos().size();
+		cdao.inserir(contato);
+		assertTrue("O contato não foi adicionado.", tamanhoanterior<=cdao.lerTodos().size());
+	}
 }
+
